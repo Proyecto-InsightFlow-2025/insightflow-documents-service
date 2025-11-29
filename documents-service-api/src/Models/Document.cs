760c10056db67f684ff68e7d6896ffffd0d2acac
ignacio Carvajal <ignacio.carvajal@alumnos.ucn.cl>
2025-11-29 13:14:19 -0300

    feat: added models and Dtos

---
+++ documents-service-api/src/Models/Document.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace documents_service_api.src.models
+{
+    public class Document
+    {
+        public Guid id { get; set; } = Guid.NewGuid();
+        public string title { get; set; }
+        public string icon { get; set; }
+        public Guid workspace_id { get; set; }
+        public object content { get; set; } = new List<object>();
+        public bool soft_deleted { get; set; } = false;
+
+    }
+}
